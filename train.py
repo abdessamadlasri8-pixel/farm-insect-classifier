7e06a4686f9c3e21c38a52e573aaae8d568b54c1
abdessamadlasri8-pixel <abdessamadlasri8@gmail.com>
2026-05-30 10:22:18 +0100

    Update train.py

--- train.py
+++ train.py
@@ -103,6 +103,5 @@ for epoch in range(NUM_EPOCHS):
 with open("class_names.json", "w") as f:
     json.dump(class_names, f)
 
-print(f"")
-Best Val Acc: {best_val_acc:.4f}")
+print(f"Best Val Acc: {best_val_acc:.4f}")
 print("Saved best_model.pth and class_names.json")
